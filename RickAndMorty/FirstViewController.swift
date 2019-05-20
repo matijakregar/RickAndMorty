fefcee4659ba8aca7cae1914ca55a727d938474b
Matija Kregar <matija.kregar@gmail.com>
2019-05-20 10:48:37 +0200

    Initial Commit

---
+++ RickAndMorty/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RickAndMorty
+//
+//  Created by Matija Kregar on 20/05/2019.
+//  Copyright © 2019 Matija Kregar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
