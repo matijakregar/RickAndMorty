a8d05133eeec44510ca2abe2d65c1e4ecbf7d38a
Matija Kregar <matija.kregar@gmail.com>
2019-05-21 11:38:03 +0200

    Build a URLRequest for GraphQL queries.

---
+++ RickAndMorty/Networking/HTTPMethod.swift
@@ -0,0 +1,23 @@
+//
+//  HTTPMethod.swift
+//  RickAndMorty
+//
+//  Created by Matija Kregar on 21/05/2019.
+//  Copyright © 2019 Matija Kregar. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+	
+	case options = "OPTIONS"
+	case get     = "GET"
+	case head    = "HEAD"
+	case post    = "POST"
+	case put     = "PUT"
+	case patch   = "PATCH"
+	case delete  = "DELETE"
+	case trace   = "TRACE"
+	case connect = "CONNECT"
+	
+}
